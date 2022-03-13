054226e354e995003a8b9e16cc18820e0d03e039
Ross <rossmorrison53@yahoo.com>
2022-03-13 14:20:56 -0700

    ok

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const cors = require('cors')
 const fs = require('fs-extra')
 
